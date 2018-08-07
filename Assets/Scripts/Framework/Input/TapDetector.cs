7a7a67ee73a593e0b35c83023ff09fd24047f125
Evgenii Esaulenko <evgenii.esaulenko@gmail.com>
2018-08-07 09:24:10 +0300

    Initial commit

---
+++ Assets/Scripts/Framework/Input/TapDetector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Framework.Input
+{
+    public class TapDetector
+    {
+        private readonly float _distanceLimit;
+        private readonly float _timeLimit;
+
+        private float _touchDownTime;
+        private Vector3 _touchDownPosition;
+
+        public TapDetector(float distanceLimit = 0.25f, float timeLimit = 0.25f)
+        {
+            _distanceLimit = distanceLimit;
+            _timeLimit = timeLimit;
+        }
+
+        public void RegisterTouchDown(PointerEventData eventData)
+        {
+            _touchDownTime = Time.time;
+            _touchDownPosition = eventData.position;
+        }
+
+        public bool RegisterTouchUp(PointerEventData eventData)
+        {
+            if (Time.time - _touchDownTime < _timeLimit)
+            {
+                var distance = Vector2.Distance(_touchDownPosition, eventData.position);
+                var deltaInInches = distance / Screen.dpi;
+                if (deltaInInches < _distanceLimit)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
