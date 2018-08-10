3756a80061273adfe3355acb5ca33abb3592e153
Evgenii Esaulenko <evgenii.esaulenko@gmail.com>
2018-08-10 17:15:55 +0300

    Improved visuals

---
+++ Assets/Editor/GameEditor.cs
@@ -0,0 +1,23 @@
+﻿using Game.Data;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class GameEditor
+    {
+        [MenuItem("Game/Start Game")]
+        public static void StartGame()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Main.unity");
+            EditorApplication.isPlaying = true;
+        }
+
+        [MenuItem("Game/Delete Game data")]
+        public static void ClearGameData()
+        {
+            FileUtil.DeleteFileOrDirectory(Application.persistentDataPath + "/" + GameData.FileName);
+        }
+    }
+}
--- Assets/Scripts/Game/Configuration/BallsStorage.cs
+++ Assets/Scripts/Game/Configuration/BallsStorage.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections.Generic;
-using Game.Gameplay;
+﻿using System;
+using System.Collections.Generic;
 using Game.Gameplay.Objects;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Game.Configuration
 {
     [CreateAssetMenu(fileName = "BallsStorage", menuName = "Configuration/BallsStorage")]
     public class BallsStorage : ScriptableObject
     {
+        [NonSerialized] private int _index = -1;
+
         public List<Ball> Balls = new List<Ball>();
 
         public Ball GetNextBall()
         {
-            return Balls[Random.Range(0, Balls.Count)];
+            if (Balls.Count > 1)
+            {
+                int index;
+                do
+                {
+                    index = Random.Range(0, Balls.Count);
+                }
+                while (index == _index);
+
+                _index = index;
+                return Balls[_index];
+            }
+
+            return Balls[0];
         }
     }
 }
